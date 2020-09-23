6d416d76055d015844a9ebef7a9f7d5c63647d4a
Abhishek Srivastav <abhisrivastav551@gmail.com>
2020-09-23 23:53:27 +0530

    landing svg

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 500) {
-          $(".navbar-dark").css("background" , "linear-gradient(135deg, #4A0C59 4.2%, #CB42B5 100%)");
+          $(".navbar-dark").css("background" , "#F0D1F0");
         }
   
         else{
