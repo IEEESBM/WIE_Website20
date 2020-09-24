730797acc0fb4865f134c986ce07398260ad7df4
ujjayantprakash <ujjayantprakash>
2020-09-24 11:02:57 +0530

    articles pushed

--- js/articles.js
+++ js/articles.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 50) {
-          $(".navbar-dark").css("background" , "linear-gradient(135deg, #4A0C59 4.2%, #CB42B5 100%)");
+          $(".navbar-dark").css("background" , "#BC14B8");
         }
   
         else{
