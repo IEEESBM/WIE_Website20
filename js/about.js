889671bfa8dcead01b33d43860fd5be14a1ec468
Ishika Agarwal <agarwalishika20@gmail.com>
2020-10-01 11:27:07 +0530

    footer and starting image added

--- js/about.js
+++ js/about.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
-        if (scroll > 500) {
-          $(".navbar-dark").css("background" , "linear-gradient(135deg, #4A0C59 4.2%, #CB42B5 100%)");
+        if (scroll > 400) {
+          $(".navbar-dark").css("background" , "#F0D1F0");
         }
   
         else{
