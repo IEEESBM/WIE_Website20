539d35a750399969eea3cdcfaf77717a8a773c08
DristiB <dristi.biyani@gmail.com>
2020-10-03 23:06:16 +0530

    sixth

---
+++ js/achievements.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 400) {
+          $(".navbar-dark").css("background" , "#F0D1F0");
+        }
+  
+        else{
+            // $(".navbar-dark").css("background" , "#333");
+            $(".navbar-dark").css("background", "none");  	
+        }
+    })
+})
