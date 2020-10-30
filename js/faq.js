191b6f5748e5c51dc94e2bbc2b6c850124c1989b
anushree bhat <anushreebhat2001@gmail.com>
2020-10-30 19:15:45 +0530

    portrait mode final

--- js/faq.js
+++ js/faq.js
@@ -1,9 +1,42 @@
-$("#accordion ul li").each(function(){
-    var trigger= $(this).find('a')
-    var acc_text = $(this).find('.accordion')
-    $(trigger).click(function(){
-      
-        $(acc_text).slideToggle()
+    $(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 500) {
+          $(".navbar-dark").css("background" , "#F0D1F0");
+        }
   
+        else{
+            // $(".navbar-dark").css("background" , "#333");
+            $(".navbar-dark").css("background", "none");  	
+        }
+    })
+})
+
+    
+    document.addEventListener("DOMContentLoaded", function(event) { 
+    
+    
+    var acc = document.getElementsByClassName("accordion");
+    var panel = document.getElementsByClassName('panel');
+    
+    for (var i = 0; i < acc.length; i++) {
+        acc[i].onclick = function() {
+            var setClasses = !this.classList.contains('active');
+            setClass(acc, 'active', 'remove');
+            setClass(panel, 'show', 'remove');
+    
+            if (setClasses) {
+                this.classList.toggle("active");
+                this.nextElementSibling.classList.toggle("show");
+            }
+        }
+    }
+    
+    function setClass(els, className, fnName) {
+        for (var i = 0; i < els.length; i++) {
+            els[i].classList[fnName](className);
+        }
+    }
+    
     });
-});
+ 
